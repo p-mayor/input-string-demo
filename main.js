22aad1343629cb8550ed565e2ad76c0be25536a3
tamburkey <petem14@gmail.com>
2020-05-05 21:20:12 -0400

    clean up / add output

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-let testStringElement = document.querySelector("#test-string")
-
-let userInputStringElement = document.querySelector("#input-string")
+let inputElement1 = document.querySelector("#input-1")
+let inputElement2 = document.querySelector("#input-2")
 
 let submitButton = document.querySelector("#submit")
 
+let outputElement = document.querySelector("#output")
+
 submitButton.addEventListener("click", function(){
-    let inputString = userInputStringElement.value 
+    let inputString1 = inputElement1.value
+    let inputString2 = inputElement2.value
     
-    if(testStringElement.value.includes(inputString)){
+    if(inputString2.includes(inputString1)){
         console.log("it is included")
+        outputElement.innerHTML = "yes"
     } else {
         console.log("it is not included")
+        outputElement.innerHTML = "no"
     }
 })
 
